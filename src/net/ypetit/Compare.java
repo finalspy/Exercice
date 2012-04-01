a91bf94598d2abf446790a273958363b245ad7ae
Yann PETIT <yann.petit@gmail.com>
2012-04-02 00:11:51 +0200

    add comments for TODO tasks, and empty Junit test class

--- src/net/ypetit/Compare.java
+++ src/net/ypetit/Compare.java
@@ -3,11 +3,15 @@ package net.ypetit;
 public class Compare {
 
 	/**
+	 * The goal of this project is to compare two XML files describing file systems structures and detects diffs.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Compare launched !");
-
+		// TODO check program usage
+		// TODO load input files 
+		// TODO process to detect differences (additions, modifications, deletions)
+		// TODO render results
 	}
 
 }
