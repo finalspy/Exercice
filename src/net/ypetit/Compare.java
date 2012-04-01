5434f96fdb32e44e7c3bd856ea6ae7b33ee6d2ae
Yann PETIT <yann.petit@gmail.com>
2012-04-02 01:20:22 +0200

    add jdom jar, modify build script and implement method to load xml files

--- src/net/ypetit/Compare.java
+++ src/net/ypetit/Compare.java
@@ -1,5 +1,12 @@
 package net.ypetit;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jdom.Document;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
 public class Compare {
 
 	/**
@@ -12,6 +19,26 @@ public class Compare {
 		// TODO load input files 
 		// TODO process to detect differences (additions, modifications, deletions)
 		// TODO render results
+	}
+	
+	/**
+	 * This method is used to read an xml file and load a Dom Document from it.
+	 * @param path Path of the xml file we want to load.
+	 * @return
+	 */
+	public static Document loadFile(File path) {
+		Document document = null;
+		try {
+			SAXBuilder sxb = new SAXBuilder();
+			document = sxb.build(path);
+		} catch (IOException e) {
+			System.out.println("Error reading file " + e.getMessage());
+			e.printStackTrace();
+		} catch (JDOMException e) {
+			System.out.println("Error building JDOM " + e.getMessage());
+			e.printStackTrace();
+		}
+		return document;
 	}
 
 }
